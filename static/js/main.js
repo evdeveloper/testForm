4711459ea86c274dd07e82ca709d5af2dc5e23ca
Evgeny <mushnikovz@mail.ru>
2019-11-06 09:17:07 +0300

    Form
    login form

---
+++ static/js/main.js
@@ -0,0 +1,20 @@
+$(document).ready(function () {
+    svg4everybody({});
+
+    function passShow() {
+        $('.user__pass-icon').on('click',function(){
+            var inputPass = $('#pass');
+            if(inputPass.attr('type') === 'password'){
+                inputPass.removeAttr('type');
+                inputPass.attr('type','text');
+                $('.user__pass-icon').html('<i id="i" class="fa fa-eye-slash">')
+            }else {
+                inputPass.attr('type','password');
+                $('.user__pass-icon').html('<i id="i" class="fa fa-eye">')
+            }
+        });
+    };
+
+    passShow();
+
+});
